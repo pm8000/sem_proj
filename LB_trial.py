887d98a22ca1905c45ea3e5bed04bcc3b51d1672
muepasca <pascal.jan.mueller@gmail.com>
2021-11-19 22:35:56 +0100

    added source term

--- LB_trial.py
+++ LB_trial.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import LB_trial_fun as fun
+import LB_source as src
 
 L=100 #domain length in lattice points
 dt=1 #time step
@@ -17,28 +18,35 @@ cv=718 #specific heat coefficient for constant density (air)
 gamma=1.4 #isentropic coefficient (air)
 Pr=0.7 #Prandtl number
 mu=1.5e-5 #dynamic viscosity (air)
+d=0.01 #pipe diameter
+l_f=0.026 #air
 
 R=cv*(gamma-1)
 rho_in=p_in/(R*(T_in+273)) #inlet density
 rho_init=p_init/(R*(T_init+273)) #initial density
 E_init=cv*T_init + 0.5*u_init**2
+alpha=src.get_alpha(u_in, d, mu/rho_in, Pr, l_f)
 
 f=np.zeros([3,L]) #first row with velocity -c, second row with velocity 0 and third row with velocity c
 f_eq=np.zeros([3,L])
 g=np.zeros([3,L])
 g_eq=np.zeros([3,L])
 g_star=np.zeros([3,L])
+source=np.zeros([3,L])
+source_last=np.zeros(L)
 rho=np.zeros(L) #density
 u=np.zeros(L) #velocity
 E=np.zeros(L) #total energy
 U=np.zeros(L) #internal energy
 T=np.zeros(L) #temperature
+T_wall=np.zeros(L) #wall temperture
 omega=np.zeros(L)
 omega_1=np.zeros(L)
 #initial condition
 rho[:]=rho_init
 u[:]=u_init
 T[:]=T_init
+T_wall[:]=T_init
 U[:]=(T_init*cv)
 E[:]=U[:]+u[:]**2/2
 
@@ -72,6 +80,7 @@ for t in range(nt):
     fun.get_f_eq(f_eq, rho, u, T, R, omega, dt, dx)
     fun.get_g_eq(g_eq, rho, u, E, T, R)
     fun.get_g_star(g_star, g_eq, g, rho, u, T, R, dt, dx)
-
+    src.get_source_populations(source, source_last, T, T_wall, alpha, d, dt)
+    
     f[:,:]=omega*f_eq[:,:] + (1-omega)*f[:,:]
-    g[:,:]=omega*g_eq[:,:] + (1-omega_1)*g[:,:] + (omega_1-omega)*g_star[:,:]
+    g[:,:]=omega*g_eq[:,:] + (1-omega_1)*g[:,:] + (omega_1-omega)*g_star[:,:] + source[:,:]

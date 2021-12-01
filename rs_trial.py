eebe438ddd6f1837d75404c4b3b0cc91a8faeafe
muepasca <pascal.jan.mueller@gmail.com>
2021-12-01 15:32:19 +0100

    changed pressure at inlet to zero gradient

--- rs_trial.py
+++ rs_trial.py
@@ -17,7 +17,7 @@ dx=0.01 #grid density
 N=math.ceil(L/dx) #number of cells
 d=0.1 #pipe diameter
 
-t_end=1 #end time
+t_end=2e-1 #end time
 cfl=0.5 #cfl number to define time step
 
 alpha=200 #convection heat transfer coefficient
@@ -39,32 +39,23 @@ fields=np.zeros([4,N]) #0 stores density, 1 stores momentum, 2 stores energy, 3 
 fluxes=np.zeros([3, N+1]) #0 stores density flux, 1 stores momentum flux, 2 stores energy flux
 T_wall=np.zeros(N)
 T_wall[:]=20+273
-T=np.zeros(N)
-T[:]=T_amb
 source=np.zeros([3,N])
 
 #initialise fields
 
 fields[0,:]=p_amb/(R*T_amb)
-fields[1,:]=fields[0,:]*u_inlet
-fields[2,:]=eos.get_E(rho_amb, u_inlet, p_amb, gamma)
+#fields[1,:]=fields[0,:]*u_inlet
+fields[2,:]=eos.get_E(rho_amb, fields[1,:]/rho_amb, p_amb, gamma)
 fields[3,:]=p_amb
 
 #determine time step
 cs_max=eos.get_cs(p_inlet, rho_inlet)
 dt=cfl*dx/cs_max
 nt=math.ceil(t_end/dt)
-first_point=np.zeros([5,1752])
-tot_mom=0
 
-for i in range(1752):
+
+for i in range(nt):
  
-    #if i%200==0:
-    #    plt.plot(np.linspace(0.005,0.995,100),fields[1,:], label='t='+str(i*dt)+' s')
-    #print(T_list)
-    if i==2000:
-        eos.get_E(rho_inlet, u_inlet, p_inlet)
-    print("step",i)
     if (i+1)*dt>t_end:
         dt=t_end-i*dt
 
@@ -74,43 +65,5 @@ for i in range(1752):
     #add source
     src.add_source(source, alpha, d, fields, R, T_wall)
     #integrate with euler explicit
-    prt_1=dt*source[2,0]
-    prt_2=dt/dx*(fluxes[2,0]-fluxes[2,1])
     fields[:-1,:]+=(dt/dx*(fluxes[:,:-1]-fluxes[:,1:])+dt*source[:,:])
     fields[3,:]=eos.get_p(fields[0,:], fields[1,:]/fields[0,:], fields[2,:])
-    T[:]=fields[3,:]/(fields[0,:]*R)
-    
-    tot_mom+=fluxes[1,0]-fluxes[1,1]
-    first_point[0,i]=fields[0,0]
-    first_point[1,i]=fields[1,0]
-    first_point[2,i]=fields[2,0]
-    first_point[3,i]=fields[3,0]
-    first_point[4,i]=T[0]    
-
-plt.plot(first_point[0,:], label='source')
-plt.title('density')
-plt.savefig('first_cell_density_supers')
-plt.clf()
-plt.plot(first_point[1,:], label='flux difference')
-plt.title('velocity')
-plt.savefig('first_cell_velocity_supers')
-#plt.legend()
-#plt.show()
-plt.clf()
-"""
-print("density influx", inlet_flux[0])
-print("momentum influx", inlet_flux[1])
-print("energy influx", inlet_flux[2])
-"""
-plt.plot(first_point[2,:])
-plt.title('energy flux')
-plt.savefig('first_cell_energy_supers')
-plt.clf()
-plt.plot(first_point[3,:])
-plt.title('pressure')
-plt.savefig('first_cell_pressure_supers')
-plt.clf()
-plt.plot(first_point[4,:])
-plt.title('temperature')
-plt.savefig('first_cell_temperature_supers')
-plt.clf()
--- rs_trial_fluxes.py
+++ rs_trial_fluxes.py
@@ -33,20 +33,10 @@ def get_middle_state(fields, i, inlet=[0,0,0], border=0):
     #boundary cases
     if border==1:
         #construct ghost cell to the left to achieve desired flux
-        #print("i",i)
-        #print("average", fields[0,i-1])
-        #print("correction", minmod(fields[0, i-1], fields[0,i], 2*inlet[0]-fields[0,i-1]))
         rho_l=fields[0,i-1]+0.5*minmod(fields[0, i-1], fields[0,i], 2*inlet[0]-fields[0,i-1])
         u_l=fields[1,i-1]/fields[0, i-1]+0.5*minmod(fields[1,i-1]/fields[0,i-1], fields[1,i]/fields[0,i], 2*inlet[1]-fields[1,i-1]/fields[0,i-1])
         p_l=fields[3,i-1]+0.5*minmod(fields[3, i-1], fields[3,i], 2*inlet[2]-fields[3,i-1])
-        
-        #rho_l=fields[0,0]
-        #u_l=fields[1,0]/rho_l
-        #p_l=fields[3,0]
     else:
-        #print("i",i)
-        #print("average", fields[0,i-1])
-        #print("correction", minmod(fields[0,i-1], fields[0,i], fields[0,i-2]))
         rho_l=fields[0, i-1]+0.5*minmod(fields[0,i-1], fields[0,i], fields[0,i-2])
         u_l=fields[1,i-1]/fields[0,i-1]+0.5*minmod(fields[1,i-1]/fields[0,i-1], fields[1,i]/fields[0,i], fields[1,i-2]/fields[0,i-1])
         p_l=fields[3,i-1]+0.5*minmod(fields[3,i-1], fields[3,i], fields[3,i-2])
@@ -80,9 +70,7 @@ def get_middle_state(fields, i, inlet=[0,0,0], border=0):
         else:
             rho_m=(p_m-p_r)/(cs_r*cs_r)+rho_r
     E_m=eos.get_E(rho_m, u_m, p_m)
-    #print("rho_l", rho_l, "u_l", u_l, "p_l", p_l, "cs_l", cs_l)
-    #print("rho_r", rho_r,"u_r", u_r, "p_r", p_r, "cs_r", cs_r)
-    #print("rho", rho_m, "u", u_m, "p", p_m, "E", E_m)
+
     return (rho_m, u_m, p_m, E_m)
 
 def get_outlet_bc(fluxes, fields):
@@ -93,23 +81,31 @@ def get_outlet_bc(fluxes, fields):
     fluxes[0,-1]=fields[1,-1]
     fluxes[1,-1]=fields[1,-1]**2/fields[0,-1]+fields[3,-1]
     fluxes[2,-1]=fields[1,-1]/fields[0,-1]*(fields[2,-1]+fields[3,-1])
-
+    
+def no_outlet(fluxes, fields):
+    #close pipe and restrict outflow
+    #input: (4xN) array fields with stored values of density, momentum, energy and pressure, (3xN+1) array fluxes to write fluxes to
+    #output:(3xN+1) array with updated fluxes, passed by reference
+    fluxes[0,-1]=0
+    fluxes[1,-1]=eos.get_p(fields[0,-1], fields[1,-1]/fields[0,-1], fields[2,-1])
+    fluxes[2,-1]=0
+    
 def calc_inlet_bc(rho, u, p):
     #calculate inlet fluxes given by BC. Fluxes are assumed to be constant, hence only one evaluation is required
     #input: scalars inlet density, velocity and pressure
     #output: (1x3) vector with inlet fluxes
     res=np.zeros([3])
     res[0]=rho*u
-    res[1]=rho*u*u+p
+    res[1]=rho*u*u
     res[2]=u*(eos.get_E(rho, u, p)+p)
     return res
 
-def get_inflow_bc(fluxes, inlet):
+def get_inflow_bc(fluxes, inlet, fields):
     #add the inlet fluxes
     #input: (3xN+1) array to write inlet fluxes to, (1x3) vector containing fluxes
     #output: inlet fluxes added to fluxes in the first column, passed by reference
     fluxes[0,0]=inlet[0]
-    fluxes[1,0]=inlet[1]
+    fluxes[1,0]=inlet[1]+fields[3,0]
     fluxes[2,0]=inlet[2]
             
 def update_fluxes(fluxes, fields, inlet_flux, inlet):
@@ -119,7 +115,8 @@ def update_fluxes(fluxes, fields, inlet_flux, inlet):
     #       vector of length 3 (inlet) containing (in this order) inlet density, velocity and pressure
     #output: (3xN+1) array with updated fluxes, passed by reference
     
-    get_inflow_bc(fluxes, inlet_flux)
+    #inlet BC
+    get_inflow_bc(fluxes, inlet_flux, fields)
     
     for i in range(1, fluxes.shape[1]-1):
         if i==1:
@@ -131,7 +128,8 @@ def update_fluxes(fluxes, fields, inlet_flux, inlet):
         fluxes[0,i]=rho_m*u_m
         fluxes[1,i]=rho_m*u_m*u_m+p_m
         fluxes[2,i]=u_m*(E_m+p_m)
-        
+    
+    #outlet BC
     get_outlet_bc(fluxes, fields)
     
 def update_fluxes_upwind(fluxes, fields, inlet_flux, inlet):

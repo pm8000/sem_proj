b548ceb452fdb2662c0bb4e03e7b9dbaed59d1e1
muepasca <pascal.jan.mueller@gmail.com>
2021-12-09 10:49:06 +0100

    minor changes

--- rs_trial.py
+++ rs_trial.py
@@ -20,7 +20,7 @@ dx=0.01 #grid density
 N=math.ceil(L/dx) #number of cells
 d=0.1 #pipe diameter
 
-t_end=5e-1 #end time
+t_end=2.6e-1 #end time
 cfl=0.5 #cfl number to define time step
 
 alpha=200 #convection heat transfer coefficient
@@ -30,9 +30,8 @@ T_inlet=200+273
 T_amb=273+20
 p_amb=1e5
 u_inlet=10
-p_inlet=p_amb
-E_loss=10
-
+p_inlet=1*p_amb
+E_loss=0
 rho_inlet=p_inlet/(R*T_inlet)
 rho_amb=p_amb/(R*T_amb)
 
@@ -62,16 +61,17 @@ dt=cfl*dx/cs_max
 nt=math.ceil(t_end/dt)
 E_correction=E_loss*dx/dt
 first_point=np.zeros([5,1])
-E_tot=np.sum(fields[2,:])
-E_tot_hist=np.zeros(nt+1)
-E_tot_hist[0]=E_tot
-t=np.linspace(0, nt*dt, nt+1)
-
+#E_tot=np.sum(fields[2,:])
+#E_tot_hist=np.zeros(nt+1)
+#E_tot_hist[0]=E_tot
+#dE_hist=np.zeros(E_tot_hist.size-1)
+#E_flow=np.zeros(nt)
+#t=np.linspace(0, nt*dt, nt)
+#src_sum=np.zeros(22672)
 start=time.time()
 for i in range(nt):
-    if i%1e4==0:
-        plt.plot(np.linspace(0.005,0.995,100),fields[3,:], label='t='+str(i*dt)+' s')
-        1+1
+    #if i%2e3==0:
+    #    plt.plot(np.linspace(0.005,0.995,100),source[2,:], label='t='+str(i*dt)+' s')
     #print(T_list)
     if i%2000==0:
         print("step",i)
@@ -83,35 +83,45 @@ for i in range(nt):
     flux.update_fluxes(fluxes, fields, inlet_flux, inlet, E_correction)
     #add source
     src.add_source(source, alpha, d, fields, R, T_wall)
-    plt.plot(np.linspace(0,1,101),fluxes[0,:], label='rho*u')
-    plt.plot(np.linspace(0,1,101),fluxes[1,:], label='rho*u*u+p')
-    plt.plot(np.linspace(0,1,101),fluxes[2,:], label='u*(E+p)')
-    plt.legend()
-    plt.show()
+    #src_sum[i]=np.sum(source[2,:])*dt
+    #plt.plot(np.linspace(0,1,101),fluxes[0,:], label='rho*u')
+    #plt.plot(np.linspace(0,1,101),fluxes[1,:], label='rho*u*u+p')
+    #plt.plot(np.linspace(0.05,0.95,100),fields[3,:], label='pressure')
+    #plt.legend()
+    #plt.show()
     #integrate with euler explicit
     fields[:3,:]+=(dt/dx*(fluxes[:,:-1]-fluxes[:,1:])+dt*source[:,:])
     fields[3,:]=eos.get_p(fields[0,:], fields[1,:]/fields[0,:], fields[2,:])
     T[:]=fields[3,:]/(fields[0,:]*R)
     fields[4,:]=fields[1,:]/fields[0,:]
     #E_tot=np.sum(fields[2,:])
-    #out.write_energy_loss(i, dt, fields[2,-1])
+    #out.write_energy_loss(i, dt, E_tot)
     #E_tot_hist[i+1]=E_tot
-    """
-    first_point[0,i]=fields[0,0]
-    first_point[1,i]=fields[1,0]
-    first_point[2,i]=fields[2,0]
-    first_point[3,i]=fields[3,0]
-    first_point[4,i]=T[0]
-    """
+    #dE_hist[i]=E_tot_hist[i+1]-E_tot_hist[i]
+    #E_flow[i]=(fluxes[2,0]-fluxes[2,-1])*dt/dx
+    
+    #first_point[0,i]=fields[0,0]
+    #first_point[1,i]=fields[1,0]
+    #first_point[2,i]=fields[2,0]
+    #first_point[3,i]=fields[3,0]
+    #first_point[4,i]=T[0]
+    
 print("time "+str(time.time()-start))
-#plt.plot(t,E_tot_hist)
-plt.legend()
-plt.show()
+print(E_tot_hist[1:].shape)
+#plt.plot(t,src_sum, label='src')
+#plt.plot(t,dE_hist, label='dE')
+#plt.plot(t, E_flow[:], label='net flux')
+#plt.plot(t, E_flow[:]+src_sum[:], label='sum' )
+#plt.plot(E_tot_hist)
+#plt.legend(bbox_to_anchor=(1.01,1),loc='upper left', borderaxespad=0)
+#plt.show()
 """
-plt.plot(first_point[0,:], label='source')
-plt.title('density')
+plt.plot(t, first_point[3,:], label='source')
+plt.title('pressure')
+#plt.show()
 plt.savefig('first_cell_density_supers')
 plt.clf()
+
 plt.plot(first_point[1,:], label='flux difference')
 plt.title('velocity')
 plt.savefig('first_cell_velocity_supers')
